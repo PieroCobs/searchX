5dec7d92a02504b525b13fd571d991169887618e
Piero Cobs <pcobby@ymail.com>
2023-12-03 09:31:48 +0000

    Addition of ActivityIndicator to the initialization screen

--- src/components/LoaderView.tsx
+++ src/components/LoaderView.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Styles from '../styles/styles';
-import {StyleSheet, Text, View} from 'react-native';
+import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
 import Palette from '../constants/palette';
 import Typography from '../constants/typography';
+import Spacer from './Spacer';
 
 export default function LoaderView() {
   return (
     <View style={loaderStyle.container}>
+      <ActivityIndicator size="large" color={Palette.primary} />
       <Text style={Styles.h2}>SearchX</Text>
+      <Spacer height={32} />
       <Text style={loaderStyle.subText}>Initializing app</Text>
+      <Text style={loaderStyle.subText}>this may take a while</Text>
     </View>
   );
 }
